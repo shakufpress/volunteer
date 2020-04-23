8e5334e703c869e649a2598254d8098fd8a88f3a
malkiz <omieyal@gmail.com>
2020-04-23 15:55:38 +0300

    drawer layout

--- quasar-app/src/layouts/MainLayout.vue
+++ quasar-app/src/layouts/MainLayout.vue
@@ -22,7 +22,27 @@
     </q-header>
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered content-class="bg-grey-1">
-      <!-- drawer content -->
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Navigation
+        </q-item-label>
+        <q-item
+          clickable
+          v-ripple
+          v-for="menuItem in menuItems"
+          :key="menuItem.title"
+          v-bind="menuItem">
+          <q-item-section avatar>
+            <q-icon :name="menuItem.icon" />
+          </q-item-section>
+          <q-item-section>
+            {{ menuItem.title }}
+          </q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-page-container>
@@ -36,7 +56,16 @@
 export default {
   data () {
     return {
-      leftDrawerOpen: false
+      leftDrawerOpen: false,
+      // TODO: should we get the menu items from the server according to login permissions (admin / volunteer)
+      menuItems: [
+        {
+          title: 'Volunteers Dashbaord',
+          caption: 'Volunteers Dashbaord',
+          icon: 'public',
+          link: '/volunteers_dashboard'
+        }
+      ]
     }
   }
 }
