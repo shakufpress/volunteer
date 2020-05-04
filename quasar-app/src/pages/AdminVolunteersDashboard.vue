dc82641b3396b0488e509b2718a4ba334b957c16
malkiz <omieyal@gmail.com>
2020-05-04 17:37:01 +0300

    data

--- quasar-app/src/pages/AdminVolunteersDashboard.vue
+++ quasar-app/src/pages/AdminVolunteersDashboard.vue
@@ -194,6 +194,7 @@ export default {
       ],
       data: [
         {
+          id: 1,
           full_name: 'שקופי שקופוביץ',
           email: 'bla@shakuf.com',
           phone: '054-1234567',
@@ -202,6 +203,17 @@ export default {
           available_hours_per_week: 5.5,
           specialties: [{ name: 'bla', category: 'a' }, { name: 'bla bla', category: 'a' }].map(mapSpecialtiesOptions),
           notes: 'woo hoo'
+        },
+        {
+          id: 2,
+          full_name: 'mr bloop',
+          email: 'bloop@shakuf.com',
+          phone: '054-756754',
+          facebook_profile_url: 'https://www.facebook.com/bloop',
+          city: 'somewhere',
+          available_hours_per_week: 2,
+          specialties: [],
+          notes: 'na na na na na na na\n na na na\n na na na na na'
         }
       ]
     }
