41f753f529357806bb65d368bf27f0348c96c5a9
malkiz <omieyal@gmail.com>
2020-05-11 17:13:48 +0300

    about

--- quasar-app/src/pages/About.vue
+++ quasar-app/src/pages/About.vue
@@ -3,6 +3,13 @@
     <div class="q-pa-md">
       This is the Shakuf Volunteers management app.
     </div>
+    <div class="q-pa-md">
+      Developed by <a href="https://github.com/Malkiz">@Malkiz
+        <q-avatar square size="xl">
+          <img src="https://avatars1.githubusercontent.com/u/3840566" alt="@Malkiz" />
+        </q-avatar>
+      </a>
+    </div>
   </q-page>
 </template>
 
