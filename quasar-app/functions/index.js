b7222aeb77253009c1a35598351c1c47b024a8e4
Yehonatan Bitton <bitton.yehonatan@gmail.com>
2020-07-09 12:13:31 +0300

    functions: support hook for adding new user

--- quasar-app/functions/index.js
+++ quasar-app/functions/index.js
@@ -1,9 +1,13 @@
+const express = require('express');
+const cors = require('cors');
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 admin.initializeApp()
 
 const volunteersStoreName = 'volunteers'
 const statusStoreName = 'projectsRequestStatuses'
+const specialtiesStoreName = 'specialties'
+const GENERAL_CATEGORY = "כללי"
 
 function getVolunteersByEmail(email) {
   return admin.firestore().collection(volunteersStoreName)
@@ -51,3 +55,117 @@ exports.getProjectRequestsByVolunteer = functions.https.onCall((data, context) =
       return arr
     })
 })
+
+function hasSpecialty(specialties, s) {
+  let specialty = s.split(':');
+
+  try {
+    specialty = {
+      name: specialty[1].trim(),
+      category: specialty[0].trim(),
+    }
+  } catch (e)
+  {
+    specialty = {
+      name: specialty[0].trim(),
+      category: GENERAL_CATEGORY
+    }
+  }
+
+  return specialties.find(s => {
+    return s.name == specialty.name && s.category == specialty.category;
+  })
+}
+
+function getSpecialties(oldSpecialties, volunteer) {
+  let newSpecialties = [];
+  let user_specialties = [];
+  let specialties_length = oldSpecialties.length;
+  volunteer.specialties.split(',').forEach(s => {
+    specialty_id = 0;
+    let specialty
+    try {
+      specialty = hasSpecialty(oldSpecialties, s);
+    } catch (err) {
+      console.err("Specialty: " + s);
+      console.err(err.stack);
+      return
+    }
+    if (specialty) {
+      specialty_id = specialty.id;
+    } else {
+      specialty_id = specialties_length++;
+      try {
+        specialty = s.split(':');
+        specialty = {
+          id: specialty_id,
+          name: specialty[1].trim(),
+          category: specialty[0].trim(),
+        }
+      } catch (ex) {
+        specialty = {
+          id: specialty_id,
+          name: specialty[0].trim(),
+          category: GENERAL_CATEGORY,
+        };
+      }
+      newSpecialties.push(specialty);
+    }
+    user_specialties.push(specialty_id);
+  });
+
+  return [newSpecialties, user_specialties];
+}
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.post('/', async (req, res) => {
+  let specialties = []
+  try {
+    let snapshot = await (await admin.firestore().collection(specialtiesStoreName).get()).docs;
+    snapshot.forEach(doc => {
+      specialties.push({
+        id: doc.id,
+        ...doc.data()
+      })
+    })
+  } catch (err) {
+    return res.status(500).send(err.stack)
+  }
+
+  const volunteer = req.body;
+  let newspecs = []
+  let userspecs = []
+  if (volunteer.specialties != "") {
+    try {
+      [newspecs, userspecs] = getSpecialties(specialties, volunteer)
+    } catch (err) {
+      return res.status(500).send(err.stack)
+    }
+  }
+
+  try {
+    newspecs.forEach(async s => {
+      await admin.firestore().collection(specialtiesStoreName).doc(s.id + "").set(s);
+    })
+  } catch (err) {
+    return res.status(500).send(err.stack)
+  }
+
+  volunteer.specialties = userspecs;
+  volunteer.projects = [];
+  try {
+    let updateVol = await (await getVolunteersByEmail(volunteer.email)).docs
+    if (updateVol && updateVol.length == 1) {
+      await admin.firestore().collection(volunteersStoreName).doc(updateVol[0].id).update(volunteer)
+    } else {
+      await admin.firestore().collection(volunteersStoreName).add(volunteer);
+    }
+  } catch (err) {
+    return res.status(500).send(err.stack)
+  }
+  return res.status(201).json(volunteer);
+});
+
+exports.addVolunteerFromSheet = functions.https.onRequest(app);
