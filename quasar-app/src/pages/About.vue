61c529b9861ff24b2c650466529bb65efc4ebe0c
malkiz <omieyal@gmail.com>
2020-07-27 22:40:44 +0300

    add about text

--- quasar-app/src/pages/About.vue
+++ quasar-app/src/pages/About.vue
@@ -1,13 +1,25 @@
 <template>
-  <q-page>
-    <div class="q-pa-md">
-      This is the Shakuf Volunteers management app.
+  <q-page align="center">
+    <div class="q-pa-md" dir="rtl">
+      <p>
+תודה שנרשמת להתנדבות אצלנו ב"שקוף"!<br><br>
+
+הקמנו מערכת מיוחדת לניהול משימות התנדבות - איך זה עובד?<br>
+נכנסים למערכת כדי לצפות בכל <q-btn padding="none" color="secondary" to="/tasks_dashboard" label="המשימות הפתוחות" /> <br>
+במקביל, אחת לשבוע ישלח מייל מסכם עם המשימות החדשות של השבוע. <br>
+במידה ותרצו להצטרף לאחת מהן, פשוט הירשמו למשימה במערכת.<br>
+לאחר ההרשמה, יש להמתין למייל אישור ממוביל המשימה. <br>
+המייל יסמל שנכנסתם למשימה ואתם יכולים להתחיל אותה. <br><br>
+
+תודה ענקית, ואנחנו מקווים שנזכה לעבוד יחדיו ממש בקרוב.
+      </p>
     </div>
-    <div class="q-pa-md">
-      Developed by <a href="https://github.com/Malkiz">@Malkiz
-        <q-avatar square size="xl">
+    <div class="q-pa-md fixed-bottom-right">
+      Developed by <a href="https://github.com/Malkiz">
+        <q-avatar square size="md">
           <img src="https://avatars1.githubusercontent.com/u/3840566" alt="@Malkiz" />
         </q-avatar>
+        @Malkiz
       </a>
     </div>
   </q-page>
